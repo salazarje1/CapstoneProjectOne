cc39e0cecf1aa3d7a3500dfbedcc18f5f6942870
Josue Salazar <salazar@Josues-MBP.localdomain>
2022-07-21 16:05:42 -0700

    removed import of secret

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from helper import password_setup
 
 app = Flask(__name__, static_url_path='/static')
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///capstone_db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "postgresql:///capstone_db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SECRET_KEY"] = "ABC123"
